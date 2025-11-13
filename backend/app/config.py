7d5f090f703d4e17c8ad36e80a3afa0a07f31bc6
Ahsan Khowaja <ahsankhowaja666@gmail.com>
2025-11-13 13:39:00 +0500

    updated

--- backend/app/config.py
+++ backend/app/config.py
@@ -5,7 +5,7 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     APP_NAME: str = "Gate Pass Management API"
 
-    MONGO_URI: str = os.getenv("MONGO_URI", "mongodb://localhost:27017")
+    MONGO_URI: str = os.getenv("MONGO_URI", "mongodb+srv://Ahsan12:Ahsan12@botss.rvm4jx6.mongodb.net/")
     MONGO_DB_NAME: str = os.getenv("MONGO_DB_NAME", "gatepass_db")
 
     MEDIA_ROOT: str = os.getenv("MEDIA_ROOT", "media")
